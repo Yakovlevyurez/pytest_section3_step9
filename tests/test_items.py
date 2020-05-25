99670bf304e7b53fd8b2431612e27690628c7860
Yury Iakovlev <yakovlevyurez@gmail.com>
2020-05-25 19:44:07 +0300

    the solution of the task

---
+++ tests/test_items.py
@@ -0,0 +1,15 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_add_to_basket_button(browser):
+    browser.get(link)
+    #time.sleep(30)
+    browser.implicitly_wait(3)
+
+    button = browser.find_elements_by_css_selector('.btn.btn-add-to-baske')
+    assert button, 'There is no "Add to basket" button at this window'
+    #assert button > 0, '!!!There is no "Add to basket" button at this window!!!'
+
+
